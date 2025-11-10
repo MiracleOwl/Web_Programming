6b3a8671f7b935254bf13370522e7fd2c27a8389
Valentino Cvetanovski <valentinocvetanovski18@gmail.com>
2025-11-10 13:47:38 +0100

    Lab2 od labs

--- lab/src/main/java/mk/ukim/finki/wp/lab/service/BookServiceImpl.java
+++ lab/src/main/java/mk/ukim/finki/wp/lab/service/BookServiceImpl.java
@@ -6,6 +6,7 @@ import mk.ukim.finki.wp.lab.model.Book;
 import java.util.List;
 import mk.ukim.finki.wp.lab.repository.AuthorRepository;
 import mk.ukim.finki.wp.lab.model.Author;
+
 @Service
 public class BookServiceImpl implements BookService{
 
@@ -27,7 +28,7 @@ public class BookServiceImpl implements BookService{
                 .filter(a -> a.getId().equals(authorId))
                 .findFirst()
                 .orElse(null);
-        Book book = new Book(title, genre, averageRating); // id се генерира во конструктор
+        Book book = new Book(title, genre, averageRating);
         return bookRepository.save(book);
     }
     @Override
@@ -42,5 +43,20 @@ public class BookServiceImpl implements BookService{
     @Override
     public void deleteBook(Long bookId) {
         bookRepository.deleteById(bookId);
+    }
+
+    @Override
+    public Book likeBook(Long bookId) {
+        Book book = bookRepository.findAll().stream()
+                .filter(b->b.getId().equals(bookId))
+                .findFirst()
+                .orElse(null);
+        if(book==null)
+        {
+            throw new RuntimeException("Book not found");
+        }
+        book.like();
+
+        return bookRepository.save(book);
     }
 }
